e093da21d8f39ff4714dbd58b4bfa36a2a63d4e1
EnterpriseGoose <58440950+EnterpriseGoose@users.noreply.github.com>
2025-09-25 11:36:14 -0400

    Fix - Ballot submission bug Fix - Weekly overview submission bugs

--- app/models/ballot.rb
+++ app/models/ballot.rb
@@ -5,9 +5,18 @@ class Ballot < ApplicationRecord
   validates :week, presence: true, numericality: { only_integer: true }
   validates :user_id, uniqueness: { scope: :week, message: "can only have one ballot per week" }
   validates :reasoning, presence: true, if: :voted?
+  validate :cannot_resubmit_voted_ballot, on: :update
 
   # Helper method to check if ballot has been voted on
   def voted?
     voted
+  end
+
+  private
+
+  def cannot_resubmit_voted_ballot
+    if voted_was && voted?
+      errors.add(:voted, "cannot resubmit an already voted ballot")
+    end
   end
 end
