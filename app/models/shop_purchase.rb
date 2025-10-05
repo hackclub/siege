922fb7220bd8adacbdf741d584b82b022bebbeea
EnterpriseGoose <58440950+EnterpriseGoose@users.noreply.github.com>
2025-10-05 15:10:28 -0400

    Fix - mercenary time calculation

--- app/models/shop_purchase.rb
+++ app/models/shop_purchase.rb
@@ -7,7 +7,17 @@ class ShopPurchase < ApplicationRecord
 
   scope :fulfilled, -> { where(fulfilled: true) }
   scope :unfulfilled, -> { where(fulfilled: false) }
-  scope :this_week, -> { where(purchased_at: Date.current.beginning_of_week..Date.current.end_of_week) }
+  scope :this_week, -> { 
+    # Use Siege week dates (based on event start date) instead of calendar weeks
+    week_range = ApplicationController.helpers.week_date_range(ApplicationController.helpers.current_week_number)
+    if week_range
+      week_start = Date.parse(week_range[0])
+      week_end = Date.parse(week_range[1])
+      where(purchased_at: week_start.beginning_of_day..week_end.end_of_day)
+    else
+      none
+    end
+  }
   scope :by_item, ->(item_name) { where(item_name: item_name) }
 
   def self.weekly_purchases_count(user, item_name)
