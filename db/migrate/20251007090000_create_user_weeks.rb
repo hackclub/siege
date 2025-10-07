af51326970f8ef983f597e08d4daeb58a9ae2f23
EnterpriseGoose <58440950+EnterpriseGoose@users.noreply.github.com>
2025-10-07 15:19:41 -0400

    Fix - make prod db like my migration

--- db/migrate/20251007090000_create_user_weeks.rb
+++ db/migrate/20251007090000_create_user_weeks.rb
@@ -1,17 +1,17 @@
 class CreateUserWeeks < ActiveRecord::Migration[8.0]
   def change
-    create_table :user_weeks do |t|
+    create_table :user_weeks, if_not_exists: true do |t|
       t.references :user, null: false, foreign_key: true
-      t.integer :week, null: false
       t.references :project, null: true, foreign_key: true
+      t.integer :week, null: false
       t.integer :arbitrary_offset, null: false, default: 0
       t.integer :mercenary_offset, null: false, default: 0
 
       t.timestamps
     end
 
-    add_index :user_weeks, [:user_id, :week], unique: true
-    add_index :user_weeks, :week
-    add_index :user_weeks, :project_id
+    add_index :user_weeks, [:user_id, :week], unique: true, if_not_exists: true
+    add_index :user_weeks, :week, if_not_exists: true
+    add_index :user_weeks, :project_id, if_not_exists: true
   end
 end
