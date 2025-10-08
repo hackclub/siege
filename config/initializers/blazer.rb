2319a9bbaecea68c8bf8a3fae2b4e54be1893b5a
EnterpriseGoose <58440950+EnterpriseGoose@users.noreply.github.com>
2025-10-07 22:16:42 -0400

    Fix - Blazer config

--- config/initializers/blazer.rb
+++ config/initializers/blazer.rb
@@ -1,21 +1,26 @@
-Blazer.configure do |config|
-  config.user_class = "User"
-  config.user_method = :current_user
-  config.user_name = :name
-  config.user_email = :email
-  config.user_id = :id
+begin
+  Blazer.configure do |config|
+    config.user_class = "User"
+    config.user_method = :current_user
+    config.user_name = :name
+    config.user_email = :email
+    config.user_id = :id
 
-  config.before_action do |controller|
-    raise ActionController::RoutingError, "Not Found" unless controller.current_user&.super_admin?
+    config.before_action do |controller|
+      raise ActionController::RoutingError, "Not Found" unless controller.current_user&.super_admin?
+    end
+
+    config.data_sources = {
+      "main" => { "url" => ENV["DATABASE_URL"] }
+    }
+
+    config.query_timeout = 30
+    config.cache_timeout = 5.minutes
+
+    config.smart_variables = true
+    config.smart_columns = true
   end
-
-  config.data_sources = {
-    "main" => { "url" => ENV["DATABASE_URL"] }
-  }
-
-  config.query_timeout = 30
-  config.cache_timeout = 5.minutes
-
-  config.smart_variables = true
-  config.smart_columns = true
+rescue NoMethodError
+  # Blazer not fully loaded yet (e.g., during asset precompile)
+  Rails.logger&.info "Blazer configuration skipped - not yet loaded"
 end
